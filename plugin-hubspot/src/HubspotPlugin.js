4dcf3abd646e4e3b474de9a42557dade4b4129c5
CJB <chris.berno@gmail.com>
2024-07-22 13:21:05 -0400

    fixed the all who wander bug

--- plugin-hubspot/src/HubspotPlugin.js
+++ plugin-hubspot/src/HubspotPlugin.js
@@ -18,9 +18,10 @@ export default class HubspotPlugin extends FlexPlugin {
     //If there is a task and the task has a crm ID, screenpop customer record. Otherwise show the list of contacts
     flex.CRMContainer.defaultProps.uriCallback = (task) => {
       if(task && task.attributes.crmid)
-        return `https://app-eu1.hubspot.com/contacts/26128285/contact/${task.attributes.crmid}`
+        return `https://app.hubspot.com/contacts/46308290/contact/${task.attributes.crmid}`;
       else 
-        return 'https://app-eu1.hubspot.com/contacts/26128285/contacts/list/view/all/';
+        // return 'https://app.hubspot.com/contacts/46308290/contacts/list/view/all/';
+        return 'https://v1.connie.plus';
     }
   }
 }
